78a4fe393dac19640a90533de52e5795b7198b89
Masiu games <masiugames@gmail.com>
2019-08-02 17:53:49 +0200

    V-a0.02: Added efficiency

--- Inpost manager-Console/src/com/masiuGames/inpostmanager/InpostManager.java
+++ Inpost manager-Console/src/com/masiuGames/inpostmanager/InpostManager.java
@@ -1,9 +1,17 @@
 package com.masiuGames.inpostmanager;
 
+import com.masiuGames.inpostmanager.util.Worker;
+
 public class InpostManager {
 
 	public static void main(String[] args) {
-
+		generateWorker();
 	}
-
+	
+	public static void generateWorker() {
+		Worker a = new Worker();
+		System.out.println("Payament: " + a.getPayament());
+		System.out.println("Efficiency: " + a.getEfficiency());
+	}
+	
 }
--- Inpost manager-Console/src/com/masiuGames/inpostmanager/util/Worker.java
+++ Inpost manager-Console/src/com/masiuGames/inpostmanager/util/Worker.java
@@ -8,6 +8,26 @@ public class Worker {
 		return payament;
 	}
 	public int getEfficiency() {
+		if(payament<2000)
+		{
+			efficiency = 1;
+		}
+		if(payament>=2000 && payament<4000)
+		{
+			efficiency = 2;
+		}
+		if(payament>=4000 && payament<6000)
+		{
+			efficiency = 3;
+		}
+		if(payament>=6000 && payament<8000)
+		{
+			efficiency = 4;
+		}
+		if(payament>=8000)
+		{
+			efficiency = 5;
+		}
 		return efficiency;
 	}
 	
